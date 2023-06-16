651b197e1db27590847eddd3bd46837210c4fbbb
tmaid <tmaidans@yahoo.com>
2023-06-16 13:38:23 -0400

    Laptop upload

---
+++ src/test/java/com/cydeo/tests/RandomTest.java
@@ -0,0 +1,17 @@
+package com.cydeo.tests;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class RandomTest {
+
+    public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+
+    }
+
+}
