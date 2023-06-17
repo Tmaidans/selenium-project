7173f708084c64c5f7c2c9f0077f5a390bcf5100
Tmaidans <tmaidans@yahoo.com>
2023-06-16 22:46:52 -0400

    Working on da 9 class

---
+++ src/test/java/com/cydeo/tests/day09_javafaker_driverUtil/T2_DriverUtil_Practice.java
@@ -0,0 +1,9 @@
+package com.cydeo.tests.day09_javafaker_driverUtil;
+
+import org.testng.annotations.BeforeMethod;
+
+public class T2_DriverUtil_Practice {
+
+    @BeforeMethod
+
+}
---
+++ src/test/java/com/cydeo/utilities/Driver.java
@@ -0,0 +1,56 @@
+package com.cydeo.utilities;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.time.Duration;
+
+public class Driver {
+
+    //create private constructor to remove access to this object
+
+    private Driver() {
+    }
+
+    /*
+    We make the Webdriver private because we want to close access from outside the class.
+    We are making it static because we use it in static method.
+     */
+    private static WebDriver driver;
+
+    /*
+    Create reusable utility method which will return the same driver instance once we call it.
+    - If an instance doesn't exist, it will create first, and then it will always return the same instance.
+     */
+    public static WebDriver getDriver() {
+
+        if (driver == null) {
+            String browserType = ConfigReader.getProperty("browser");
+
+
+               /*
+               Depending on the browser type returned from configuration.properties, switch statement will determine
+               the case and open matching browser .
+                */
+            switch (browserType) {
+                case "chrome":
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver();
+                    driver.manage().window().maximize();
+                    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+                case "firefox":
+                    WebDriverManager.firefoxdriver().setup();
+                    driver = new FirefoxDriver();
+                    driver.manage().window().maximize();
+                    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+            }
+
+
+        }
+
+        return driver;
+    }
+}
